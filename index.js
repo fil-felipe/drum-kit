d1c0f85708cbf99a5dccc74abe913f89e0e77a2b
fil-felipe <113887791+fil-felipe@users.noreply.github.com>
2022-11-10 21:55:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,23 +25,23 @@ function makeSound(key) {
      kickBass.play();
      break;
     case "s":
-     var snare = new Audio("../sounds/snare.mp3");
+     var snare = new Audio("./sounds/snare.mp3");
      snare.play();
      break;
     case "d":
-     var tom1 = new Audio("../sounds/tom-1.mp3");
+     var tom1 = new Audio("./sounds/tom-1.mp3");
      tom1.play();
      break;
     case "j":
-     var tom2 = new Audio("../sounds/tom-2.mp3");
+     var tom2 = new Audio("./sounds/tom-2.mp3");
      tom2.play();
      break;
     case "k":
-     var tom3 = new Audio("../sounds/tom-3.mp3");
+     var tom3 = new Audio("./sounds/tom-3.mp3");
      tom3.play();
      break;
     case "l":
-     var tom4 = new Audio("../sounds/tom-4.mp3");
+     var tom4 = new Audio("./sounds/tom-4.mp3");
      tom4.play();
      break;
     default: console.log(key)
