37e2d7feadaf33a4c6772148fc20d76c8bec4c72
fil-felipe <113887791+fil-felipe@users.noreply.github.com>
2022-11-10 20:37:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.addEventListener("keydown", function(event) {
 function makeSound(key) {
   switch (key) {
     case "w":
-     var crash = new Audio("sounds/crash.mp3");
+     var crash = new Audio("./sounds/crash.mp3");
      crash.play();
      break;
     case "a":
